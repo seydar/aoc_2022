a639dd9f76b4d0fab59868a35f2863a6d3d28388
Ari Brown <ari@airintech.com>
2022-12-01 12:59:40 -0500

    shortened the ruby based on some feedback on the haskell version

--- 01.rb
+++ 01.rb
@@ -6,22 +6,15 @@
 # your boats approach land, the Elves begin taking inventory of their supplies.
 # One important consideration is food - in particular, the number of Calories
 # each Elf is carrying (your puzzle input).
-elves = [[]]
-while line = STDIN.gets
-  line = line.chomp
 
-  # The Elves take turns writing down the number of Calories contained by the
-  # various meals, snacks, rations, etc. that they've brought with them, one
-  # item per line. Each Elf separates their own inventory from the previous
-  # Elf's inventory (if any) by a blank line.
-  #
-  # Empty line == new elf
-  if line == ""
-    elves << []
-  # Non-empty line == add to the latest elf's list of provisions
-  else
-    elves.last << line.to_i
-  end
+# The Elves take turns writing down the number of Calories contained by the
+# various meals, snacks, rations, etc. that they've brought with them, one
+# item per line. Each Elf separates their own inventory from the previous
+# Elf's inventory (if any) by a blank line.
+#
+# Empty line == new elf
+elves = STDIN.read.split("\n\n").map do |chunk|
+  chunk.split.map {|line| line.to_i }
 end
 
 AOC_PART = ARGV[0] || "one"
