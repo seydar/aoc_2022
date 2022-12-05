8cd70e042f6035ed3baefd8c2b47527db6a9de9e
Ari Brown <ari@airintech.com>
2022-12-05 15:46:10 -0500

    cleaning up day 3

--- 03/03.rb
+++ 03/03.rb
@@ -1,35 +1,38 @@
 #!/usr/bin/env ruby
 
-def priority(letters)
-  letters.map do |letter|
-    if ("A".."Z").include? letter
-      letter.ord - "A".ord + 27
-    else
-      letter.ord - "a".ord + 1
-    end
-  end.sum
-end
-
-def rucksacks(rucksacks)
-  rucksacks.split "\n"
-end
-
 def compartments(sack)
   median       = sack.size / 2
-  compartments = [sack[0..median - 1].split(""),
-                  sack[median..-1].split("")]
+  [sack[0..median - 1].split(""), sack[median..-1].split("")]
 end
 
-sacks = rucksacks STDIN.read
+def priority(letter)
+  case letter
+  when "A".."Z"
+    letter.ord - "A".ord + 27
+  when "a".."z"
+    letter.ord - "a".ord + 1
+  end
+end
 
-AOC_PART = ARGV[0] || "one"
-if AOC_PART == "one"
-  common = sacks.map {|s| compartments(s).reduce &:& }
-  puts common.map {|item| priority item }.sum
-elsif AOC_PART == "two"
-  badges = sacks.each_slice(3).map do |s1, s2, s3|
-    s1.split("") & s2.split("") & s3.split("")
+def part1(sacks)
+  # assume only one common item
+  common = sacks.map {|s| compartments(s).reduce(&:&)[0] }
+  common.map {|item| priority item }.sum
+end
+
+def part2(sacks)
+  badges = sacks.each_slice(3).map do |group|
+    group.map {|s| s.split("") }.reduce(&:&)[0]
   end
   puts badges.map {|b| priority b }.sum
+end
+
+sacks = STDIN.read.split "\n"
+
+case ARGV[0]
+when "one"
+  puts part1(sacks)
+when "two"
+  puts part2(sacks)
 end
 
