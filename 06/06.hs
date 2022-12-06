d7cfee7dc97eb2a835d214e1a2fc644747b08882
Ari Brown <ari@airintech.com>
2022-12-06 14:16:49 -0500

    updated day 6 in haskell to remove tail recursion. cleaner solution now

--- 06/06.hs
+++ 06/06.hs
@@ -1,7 +1,7 @@
 import System.IO
 import System.Environment
 import qualified Data.Set as Set
-import Debug.Trace
+import Data.List
 
 type Signal = String
 
@@ -12,20 +12,18 @@ nubOrd xs = go Set.empty xs where
    | otherwise        = x : go (Set.insert x s) xs
   go _ _              = []
 
-uniqueSubstring :: String -> Int -> Int
-uniqueSubstring str duration = helper str 0 
+uniqueSubstring :: Int -> String -> Int
+uniqueSubstring duration = helper . find (uniq . snd) . zip [duration..] . tails
   where
     uniq sub = (length . nubOrd . take duration $ sub) == duration
-    helper sub n
-      | length sub < duration = -1
-      | uniq sub              = n + duration
-      | otherwise             = helper (tail sub) (n + 1)
+    helper Nothing       = -1
+    helper (Just (n, _)) = n
 
 packetStart :: Signal -> Int
-packetStart signal = uniqueSubstring signal 4
+packetStart = uniqueSubstring 4
 
 messageStart :: Signal -> Int
-messageStart signal = uniqueSubstring signal 14
+messageStart = uniqueSubstring 14
 
 partOne = map packetStart . lines
 partTwo = map messageStart . lines
